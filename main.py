68fa157220457f8bedf045352ac7bffe8dff0c53
Kailexi <72436264+Kailexi@users.noreply.github.com>
2023-09-16 09:39:59 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+
+name = 'Vadim_Segeyevich'
+
+class Hello_Teacher:
+    def __init__(self,hello):
+        self.a  = input()
+        print(f'Hello', {self.a},'!')
+
+
+x = Hello_Teacher('Alex') # bro why
