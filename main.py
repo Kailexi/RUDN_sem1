12c8d88e8103c3bb9a6f63730f691638f1756927
Kailexi <soniccally.sonic1010@gmail.com>
2023-09-16 14:03:48 +0300

    update

--- main.py
+++ main.py
@@ -3,11 +3,17 @@ name = 'Vadim_Sergeyevich'
 
 class Hello_Teacher:
     def __init__(self,hello):
-        self.a  = input()
+        self.name  = input()
         print(f'Hello', {self.a},'!')
 
 
 x = Hello_Teacher('Alex')
+
+
+
+
+
+
 
 
 
