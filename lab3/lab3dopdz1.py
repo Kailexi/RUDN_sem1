19d8edd37574acc293d08a1057b5fef249c82ca7
Kailexi <soniccally.sonic1010@gmail.com>
2023-12-23 09:34:44 +0300

    added funny comments

--- lab3/lab3dopdz1.py
+++ lab3/lab3dopdz1.py
@@ -1,17 +1,12 @@
-count  = 0
+count = 0
 fail_count = 0
-
-
-
-
 data = []
-
-
 
 
 def askValue():
     x1 = int(input())
     return x1
+
 
 def checkValue(x1):
     check = True
@@ -24,7 +19,7 @@ def checkValue(x1):
         return check
 
     else:
-        print(f'Ответ не правильный,правильный ответ {chisla[0]*chisla[1]} ')
+        print(f'Ответ не правильный,правильный ответ {chisla[0] * chisla[1]} ')
 
         global fail_count
         fail_count += 1
@@ -32,9 +27,7 @@ def checkValue(x1):
         return check == False
 
 
-
-
-for i in range(1,5+1):
+for i in range(1, 5 + 1):
     chisla = list(map(int, input(f'Введите 2 числа\nПример: 5 6\n ').split()))
     print(f'Сколько будет {chisla[0]} * {chisla[1]} ?')
 
@@ -47,25 +40,4 @@ for i in range(1,5+1):
     if fail_count == 3:
         print('Нифига ты зачем в универ пришёл инжнерный тогда ХД')
 
-
-
 print('Игра закончена спасибо за игру!')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
